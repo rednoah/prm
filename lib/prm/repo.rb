29768b7e997e8ea50f888e7955a14cd1557a397c
Zachary Schneider <sigil.66@gmail.com>
2018-09-20 16:36:43 -0500

    Update repo.rb

--- lib/prm/repo.rb
+++ lib/prm/repo.rb
@@ -124,8 +124,8 @@ module Debian
                     end
                 end
             end
-            `ar p #{deb} control.tar.gz | tar zx -C tmp/#{tdeb}/`
-            `ar p #{deb} control.tar.xz | tar x -C tmp/#{tdeb}/`
+            `ar -t  #{deb} | grep gz && ar p #{deb} control.tar.gz | tar zx -C tmp/#{tdeb}/`
+            `ar -t #{deb} | grep xz && ar p #{deb} control.tar.xz | tar Jx -C tmp/#{tdeb}/`
 
             package_info = [
                 "Filename: #{npath}#{tdeb}",
